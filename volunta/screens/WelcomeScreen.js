17dabcc74449188ee90fcf5040c98cbdf6fb101e
Katherine Eisenbrand <keisenbrand@gmail.com>
2019-04-24 12:16:35 -0700

    added welcome screen, uploaded logo assets

---
+++ volunta/screens/WelcomeScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={require('../assets/images/logo.png')}
+            style={styles.logo}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 183,
+  },
+  logo: {
+    width: 234,
+    height: 223,
+    resizeMode: 'contain',
+  },
+});
