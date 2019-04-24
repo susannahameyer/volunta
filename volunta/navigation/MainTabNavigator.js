4f0b7c3198265ac56bafcd9cad3c09d27707afb4
Luis Fernando Varela <lfvarela@stanford.edu>
2019-04-24 12:56:52 -0700

    Replacing Home Screen with Feed Screen

--- volunta/navigation/MainTabNavigator.js
+++ volunta/navigation/MainTabNavigator.js
@@ -1,57 +1,47 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
+import FeedScreen from '../screens/FeedScreen';
 import CommunityScreen from '../screens/CommunityScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import NotificationsScreen from '../screens/NotificationsScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+const FeedStack = createStackNavigator({
+  Feed: FeedScreen
 });
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+FeedStack.navigationOptions = {
+  tabBarLabel: 'Feed',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
+    <TabBarIcon focused={focused} name={'ios-menu'} />
+  )
 };
 
 const CommunityStack = createStackNavigator({
-  Community: CommunityScreen,
+  Community: CommunityScreen
 });
 
 CommunityStack.navigationOptions = {
   tabBarLabel: 'Community',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={'ios-people'}
-    />
-  ),
+    <TabBarIcon focused={focused} name={'ios-people'} />
+  )
 };
 
 const NotificationsStack = createStackNavigator({
-  Notifications: NotificationsScreen,
+  Notifications: NotificationsScreen
 });
 
 NotificationsStack.navigationOptions = {
   tabBarLabel: 'Notifications',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={'ios-notifications'}
-    />
-  ),
+    <TabBarIcon focused={focused} name={'ios-notifications'} />
+  )
 };
 
 const ProfileStack = createStackNavigator({
@@ -69,8 +59,8 @@ ProfileStack.navigationOptions = {
 };
 
 export default createBottomTabNavigator({
-  HomeStack,
+  FeedStack,
   CommunityStack,
   NotificationsStack,
-  ProfileStack,
+  ProfileStack
 });
