ae7101c25ac40b4b94c7e917f618c56e3898d654
Luis Fernando Varela <lfvarela@stanford.edu>
2019-04-25 16:38:57 -0700

    delete constants

--- volunta/firebase/constants.js
+++
@@ -1,3 +0,0 @@
-export const FIREBASE_API_KEY = 'AIzaSyCzGXdD8RYyiHg24lQ8zBsoBQuE-MBW2Jo';
-export const FIREBASE_PROJECT_ID = 'volunta-proj-id';
-export const FIREBASE_DATABASE_URL = 'https://volunta-proj-id.firebaseio.com';
