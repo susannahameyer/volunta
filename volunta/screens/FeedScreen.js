f086a62cb08dcfd4d92387237e3966d5474acf81
Luis Fernando Varela <lfvarela@stanford.edu>
2019-04-26 13:15:57 -0700

    small bug update

--- volunta/screens/FeedScreen.js
+++ volunta/screens/FeedScreen.js
@@ -90,7 +90,7 @@ export default class FeedScreen extends React.Component {
             style={styles.flatListStyle}
             renderItem={this._renderEventCard}
             data={events}
-            onRefresh={() => this._loadEvents()}
+            onRefresh={() => this._loadData()}
             keyExtractor={(_, index) => index.toString()}
             refreshing={isRefreshing}
           />
