e3f613828dfa36194624c0a5a0f19444a507ef59
Katherine Eisenbrand <keisenbrand@gmail.com>
2019-04-29 11:50:44 -0700

    initial commit for login stack

--- volunta/navigation/MainTabNavigator.js
+++ volunta/navigation/MainTabNavigator.js
@@ -11,10 +11,15 @@ import CommunityScreen from '../screens/CommunityScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import NotificationsScreen from '../screens/NotificationsScreen';
 import EventScreen from '../screens/EventScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const LoginStack = createStackNavigator({
+  Login: LoginScreen,
+})
 
 const FeedStack = createStackNavigator({
+  Event: EventScreen,
   Feed: FeedScreen,
-  Event: EventScreen
 });
 
 FeedStack.navigationOptions = {
---
+++ volunta/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class LoginScreen extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+}
