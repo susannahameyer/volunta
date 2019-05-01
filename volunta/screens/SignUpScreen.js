765dc5c175b16dbb993af4fe6221e79861c41ff4
Katherine Eisenbrand <keisenbrand@gmail.com>
2019-04-30 22:24:04 -0700

    added signupscreen.js

---
+++ volunta/screens/SignUpScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class SignUpScreen extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Sign up</Text>
+      </View>
+    );
+  }
+}
