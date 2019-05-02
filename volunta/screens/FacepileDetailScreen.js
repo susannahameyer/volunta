abd4a74e8ef391e5df6bebe82a8c2d061ceaf06b
ihodge97 <ihodge@stanford.edu>
2019-05-01 17:43:51 -0700

    images now load when you click a facepile

--- volunta/screens/FacepileDetailScreen.js
+++ volunta/screens/FacepileDetailScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { 
+        View, 
+        Text, 
+        StyleSheet, 
+        TouchableOpacity,
+        Image 
+    } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 import { SearchBar } from 'react-native-elements';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default class FacepileDetailScreen extends React.Component {
 
@@ -9,13 +17,29 @@ export default class FacepileDetailScreen extends React.Component {
         title: navigation.getParam('title', 'Details'),
     })
 
-
-
     constructor(props) {
         super(props);
         this.state = {
             users: this.props.navigation.getParam('users')
         };
+    }
+
+    _renderUser = user => {
+        return (
+            <TouchableOpacity
+                style={styles.userDetailsContainer}>
+                <Image
+                    style={styles.profileImage}
+                    source={user.photo}/>
+                <Text style={styles.profileText}>
+                    {user.name}
+                </Text>
+                <Icon
+                    name={'right'}
+                    size={30}/>
+            </TouchableOpacity>
+        )
+        
     }
 
     render() {
@@ -28,9 +52,11 @@ export default class FacepileDetailScreen extends React.Component {
                     round
                     containerStyle={styles.searchContainerStyle}
                     inputContainerStyle={styles.searchInputContainerStyle}/>
-                <Text>
-                    You made it here!
-                </Text>
+                <FlatList
+                    data={this.state.users}
+                    renderItem={({item}) => this._renderUser(item)}
+                />
+
             </View>
         );
     }
@@ -46,4 +72,25 @@ const styles = StyleSheet.create({
     searchInputContainerStyle: {
       backgroundColor: '#E8E8E8'
     },
+    userDetailsContainer: {
+        height: 78,
+        borderBottomColor: '#DCDCDC',
+        borderBottomWidth: 1,
+        flex:1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    profileImage: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        marginLeft:15,
+        marginRight:12,
+    },
+    profileText: {
+        fontFamily: 'montserrat',
+        fontSize: 14,
+        color: 'black',
+        fontWeight: 'bold',
+    }
   });
