8d345d2e6726b73bb47d326bd98bc2e091f770fc
Susannah Meyer <smeyer7@stanford.edu>
2019-05-06 10:37:04 -0700

    initial commit

---
+++ volunta/components/EventPageHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default class EventPageHeader extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { eventTitle, organizationName, organizationLogo } = this.props;
+    return;
+    <View />;
+  }
+}
+
+const styles = StyleSheet.create({});
--- volunta/screens/EventScreen.js
+++ volunta/screens/EventScreen.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import EventPageHeader from '../components/EventPageHeader';
 
 export default class EventScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      event: this.props.navigation.getParam('event')
+      event: this.props.navigation.getParam('event'),
     };
   }
 
   render() {
     const event = this.state.event;
-    return (
-      <View>
-        <Text>Event: {event.title}</Text>
-      </View>
-    );
+    return <View>{/* <Text>Event: {event.title}</Text> */}</View>;
   }
 }
+
+const styles = StyleSheet.create({});
