7cd7f3b4c8d149564dda3af264dd6b0f75ee0cbb
Katherine Eisenbrand <keisenbrand@gmail.com>
2019-05-11 18:02:12 -0700

    remove todo

--- volunta/screens/AuthLoadingScreen.js
+++ volunta/screens/AuthLoadingScreen.js
@@ -28,8 +28,6 @@ export default class AuthLoadingScreen extends React.Component {
 
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    // TODO: uncomment token check
-    //this.props.navigation.navigate('Main');
     this.props.navigation.navigate(userToken ? 'Main' : 'Auth');
   };
 
