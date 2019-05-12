9f549b4ede81c703fa2d189a8446ec0a3b9b970a
Katherine Eisenbrand <keisenbrand@gmail.com>
2019-05-11 17:55:16 -0700

    uncomment apikey

--- volunta/firebase/firebase.js
+++ volunta/firebase/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import * as c from './fb_constants';
-// import FIREBASE_API_KEY from './apiKey';
+import FIREBASE_API_KEY from './apiKey';
 
 // Initialize Firebase
 const config = {
-  apiKey: 'AIzaSyCzGXdD8RYyiHg24lQ8zBsoBQuE-MBW2Jo',
+  apiKey: FIREBASE_API_KEY,
   projectId: c.FIREBASE_PROJECT_ID,
   databaseURL: c.FIREBASE_DATABASE_URL
 };
