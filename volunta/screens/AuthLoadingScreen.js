352640bfd9b8090f8e5577dde81f12f17105cbfa
Katherine Eisenbrand <keisenbrand@gmail.com>
2019-05-13 10:42:34 -0700

    remove asyncstorage

--- volunta/screens/AuthLoadingScreen.js
+++ volunta/screens/AuthLoadingScreen.js
@@ -13,23 +13,14 @@ import * as firebase from 'firebase';
 export default class AuthLoadingScreen extends React.Component {
   constructor(props) {
     super(props);
-    this._bootstrapAsync();
   }
 
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
-      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+      console.log(user);
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp');
     })
   }
-
-  // Fetch the token from storage then navigate to our appropriate place
-  _bootstrapAsync = async () => {
-    const userToken = await AsyncStorage.getItem('userToken');
-
-    // This will switch to the App screen or Auth screen and this loading
-    // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(userToken ? 'Main' : 'Auth');
-  };
 
   // Render any loading content that you like here
   render() {
