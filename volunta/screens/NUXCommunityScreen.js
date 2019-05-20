0a369403b2026a48abe4cff86e4a4295bfcc6c47
Susannah Meyer <smeyer7@stanford.edu>
2019-05-20 10:48:51 -0700

    initial community NUX

--- volunta/screens/NUXCommunityScreen.js
+++ volunta/screens/NUXCommunityScreen.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import {
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default class NUXCommunityScreen extends React.Component {
-
   constructor(props) {
     super(props);
   }
 
   // Function we pass to Log In button, pushes login screen onto stack
   _onPressContinue = event => {
-    this.props.navigation.navigate('Interests')
+    this.props.navigation.navigate('Interests');
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Welcome to volunta! Please choose the community you want 
-          to register your account with.
+        <Text style={styles.welcomeText}>Welcome to volunta!</Text>
+        <Text style={styles.detailText}>
+          Please choose the community you want to register your account with.
         </Text>
         <View>
+          {' '}
+          style={styles.buttonContainer}
           <TouchableOpacity onPress={this._onPressContinue}>
             <View style={styles.button}>
               <Text style={styles.buttonText}>continue</Text>
@@ -48,7 +43,7 @@ const styles = StyleSheet.create({
     width: 202,
   },
   buttonText: {
-    color: "#FFFFFF",
+    color: '#FFFFFF',
     fontFamily: 'montserrat',
     fontSize: 18,
     fontWeight: 'normal',
@@ -56,7 +51,18 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    marginHorizontal: 25,
   },
+  welcomeText: {
+    fontFamily: 'montserrat',
+    fontSize: 24,
+    marginTop: 80,
+  },
+  detailText: {
+    textAlign: 'center',
+    fontFamily: 'montserrat',
+    fontSize: 24,
+    marginTop: 35,
+  },
+  buttonContainer: {},
 });
