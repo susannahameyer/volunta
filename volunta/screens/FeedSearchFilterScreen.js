0b359c4fa57a54acd2410f08143df0ca3c81a69f
Cody Hankins <chankinsoft@gmail.com>
2019-05-20 12:16:20 -0700

    added slider bar

--- volunta/screens/FeedSearchFilterScreen.js
+++ volunta/screens/FeedSearchFilterScreen.js
@@ -5,13 +5,19 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Slider
 } from 'react-native';
-import AssetFilePaths from '../constants/AssetFilePaths';
 
 export default class WelcomeScreen extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+        minDistance: 1,
+        maxDistance: 50,
+        currDistance: 10,
+        interests: []
+    }
   }
 
   // Function we pass to Log In button, pushes login screen onto stack
@@ -24,7 +30,27 @@ export default class WelcomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> Hello </Text>
+        <Text style={styles.header}> Distance </Text>
+        <View style={styles.sliderBar}>
+            <Slider
+                style={{ width: '90%'}}
+                step={1}
+                minimumValue={this.state.minDistance}
+                maximumValue={this.state.maxDistance}
+                value={this.state.currDistance}
+                onValueChange={val => this.setState({ currDistance: val })}
+                thumbTintColor='rgb(252, 228, 149)'
+                maximumTrackTintColor='#d3d3d3' 
+                minimumTrackTintColor='#0081AF'
+            />
+            <View style={styles.textCon}>
+                <Text style={styles.colorGrey}>{this.state.minDistance} mi</Text>
+                <Text style={styles.colorBlue}>
+                    {this.state.currDistance + 'mi'}
+                </Text>
+                <Text style={styles.colorGrey}>{this.state.maxDistance} mi</Text>
+            </View>
+        </View>
       </View>
     );
   }
@@ -32,9 +58,28 @@ export default class WelcomeScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
   },
+  header: {
+    fontSize: 24,
+  },
+  sliderBar: {
+    marginLeft: '7%'
+  },
+  distanceBlock: {
+    width: '100%',
+  },
+  textCon: {
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  colorGrey: {
+    color: '#d3d3d3'
+  },
+  colorBlue: {
+    color: '#0081AF'
+  }
+
 });
