95b971e862a4034f67c348c0f8678aa5ebc46cff
Luis Fernando Varela <lfvarela@stanford.edu>
2019-05-21 00:13:48 -0700

    Revert "small bug"
    This reverts commit f01fcb3ec63ae74ae4185a0f2a305de5ecccf3fb.

--- volunta/screens/FeedScreen.js
+++ volunta/screens/FeedScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, FlatList, View, Dimensions } from 'react-native';
 import { EventCard } from '../components';
 import { SearchBar } from 'react-native-elements';
+import * as c from '../firebase/fb_constants';
 import { DefaultDict, distance, formatDist } from '../utils';
 import { Location } from 'expo';
 import {
@@ -10,7 +11,6 @@ import {
   updateUserInterestedEvents,
   getNumGoingForAllEvents,
 } from '../firebase/api';
-import * as firebase from 'firebase';
 
 export default class FeedScreen extends React.Component {
   _isMounted = false;
